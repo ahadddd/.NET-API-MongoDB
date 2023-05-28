f44545c9b46ad975dde7f194c63242169fc30bb6
ahadddd <ahad.shehbaz19@gmail.com>
2023-05-29 00:44:44 +0500

    Re-commit all

--- Program.cs
+++ Program.cs
@@ -1,10 +1,15 @@
+using Microsoft.Extensions.DependencyInjection.Extensions;
 using WebAPIMongo.Configurations;
+using WebAPIMongo.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-builder.Services.Configure<DbSettings>(builder.Configuration.GetSection(key: "MongoDb"));
+builder.Services.Configure<DbSettings>(builder.Configuration.GetSection(key: "DbSettings"));
 builder.Services.AddControllers();
+builder.Services.AddSingleton<DriverService>();
+builder.Services.AddLogging(logging => logging.AddConsole());
+builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
@@ -21,6 +26,8 @@ if (app.Environment.IsDevelopment())
 app.UseHttpsRedirection();
 
 app.UseAuthorization();
+
+app.UseCors(policy => policy.AllowAnyHeader().AllowAnyMethod().AllowAnyOrigin());
 
 app.MapControllers();
 
